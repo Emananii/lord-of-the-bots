c0bf6cb7209f6efbc006bf1c172b242a624310e0
Emmanuel Wambugu Ndiritu <emmanuelwambugu5@gmail.com>
2025-04-22 15:12:23 +0300

    Changed end of paragraph word from Lord to Commander.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="header">
       <h1 className="app-title">Bot Battlr: Lord of the Bots</h1>
-      <p className="tagline">The Universe Awaits Your Bot Army, Lord.</p>
+      <p className="tagline">The Universe Awaits Your Bot Army, Commander.</p>
     </header>
   )
 }
